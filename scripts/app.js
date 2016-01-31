86f9dc13e5359774dd58bfc7d366bfed9445b76d
Nathan <natecbd@gmail.com>
2016-01-30 20:21:52 -0800

    holy shit it has been 2 months

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,31 @@
 $(document).ready(function () {
  
 
-//var waypoint = new Waypoint({
-//  element: $('.my-face'),
-//  handler: function(direction) {
-//    $('.my-face').css("display","inline")
-//    console.log(direction)
-//  }
-//})
+var waypointJams = new Waypoint({
+    element: $('.ups'),
+    handler: function(direction) {
+    if(direction === "down") {
+        $('.nav-color').css("animation-name","fade-in")
+        $('.nav-color').css("background-color","rgb(58,23,63)")
+    } else if(direction === "up") {
+        $('.nav-color').css("animation-name","fade-out")
+    }
+  },
+    offset: '200px'
+})
 
-    $('nav.nav-bar a').smoothScroll();
+var waypointBand = new Waypoint({
+    element: $('.cascade'),
+    handler: function(direction) {
+    if(direction === "down") {
+        $('.nav-color').css("animation-name","purple-to-crimson")
+    } else if(direction === "up") {
+        $('.nav-color').css("animation-name","crimson-to-purple")
+    }
+  },
+    offset: '200px'
+})
+
+$('nav.nav-bar a').smoothScroll();
    
 });
