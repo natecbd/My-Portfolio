f7809a3ace5e0dd27828fbc186e5e7abec6932e7
Nathan Goodman <natecbd@natecbd.com>
2018-12-19 17:15:26 -0800

    js update

--- scripts/app.js
+++ scripts/app.js
@@ -486,3 +486,181 @@ window.onload = function(){
     return document.getElementById(a);
   }
 };
+
+  //background
+  // var cssBG = ''
+  // + '\n\n\t<span class="target">a </span>{'
+  // + '\n\t  <span class="character">text-decoration</span>: <span class="primary">none</span> ;'
+  // + '\n\t}'
+  // + '\n\t'
+  // + '\n\t<span class="target">.nav-bar</span> {'
+  // + '\n\t  <span class="character">position</span>: <span class="primary">fixed</span>;'
+  // + '\n\t  <span class="character">z-index</span>: <span class="value">999</span>;'
+  // + '\n\t  <span class="character">top</span>: <span class="value">0%</span>;'
+  // + '\n\t  <span class="character">width</span>: <span class="value">100%</span>;'
+  // + '\n\t  <span class="character">height</span>: <span class="value">70px</span>;'
+  // + '\n\t  <span class="character">list-style-type</span>: <span class="primary">none</span>;'
+  // + '\n\t  <span class="character">margin</span>: <span class="value">0</span>;'
+  // + '\n\t  <span class="character">padding</span>: <span class="value">0</span>;'
+  // + '\n\t  <span class="character">overflow</span>: <span class="primary">hidden</span>;'
+  // + '\n\t  <span class="character">-webkit-user-select</span>: <span class="primary">none</span>;'
+  // + '\n\t  <span class="character">   -moz-user-select</span>: <span class="primary">none</span>;'
+  // + '\n\t  <span class="character">    -ms-user-select</span>: <span class="primary">none</span>;'
+  // + '\n\t  <span class="character">        user-select</span>: <span class="primary">none</span>;'
+  // + '\n\t}'
+  // + '\n\t'
+  // + '\n\t<span class="target">.nav-item</span> {'
+  // + '\n\t  <span class="character">float</span>: <span class="primary">right</span>;'
+  // + '\n\t  <span class="character">text-align</span>: <span class="primary">left</span>;'
+  // + '\n\t  <span class="character">vertical-align</span>: <span class="primary">middle</span>;'
+  // + '\n\t  <span class="character">line-height</span>: <span class="value">350%</span>;'
+  // + '\n\t  <span class="character">font-size</span>: <span class="value">20px</span>;'
+  // + '\n\t  <span class="character">height</span>: <span class="value">70px</span>;'
+  // + '\n\t  <span class="character">margin-left</span>: <span class="value">30px</span>;'
+  // + '\n\t  <span class="character">margin-right</span>: <span class="value">30px</span>;'
+  // + '\n\t}'
+  // + '\n\t'
+  // + '\n\t<span class="target">.nav-link</span> {'
+  // + '\n\t  <span class="character">font-weight</span>: <span class="primary">bold</span>;'
+  // + '\n\t  <span class="character">display</span>: <span class="primary">inline-block</span>;'
+  // + '\n\t  <span class="character">text-decoration</span>: <span class="primary">none</span>;'
+  // + '\n\t  <span class="character">text-transform</span>: <span class="primary">capitalize</span>;'
+  // + '\n\t}'
+  // + '\n\t'
+  // + '\n\t<span class="target">.navb, .navc</span> {'
+  // + '\n\t  <span class="character">opacity</span>: <span class="value">0</span>;'
+  // + '\n\t  <span class="character">color</span>: <span class="primary">white</span>;'
+  // + '\n\t  <span class="character">-webkit-animation-duration</span>: <span class="value">.75s</span>;'
+  // + '\n\t  <span class="character">   -moz-animation-duration</span>: <span class="value">.75s</span>;'
+  // + '\n\t  <span class="character">     -o-animation-duration</span>: <span class="value">.75s</span>;'
+  // + '\n\t  <span class="character">        animation-duration</span>: <span class="value">.75s</span>;'
+  // + '\n\t  <span class="character">-webkit-animation-fill-mode</span>: <span class="primary">forwards</span>;'
+  // + '\n\t  <span class="character">   -moz-animation-fill-mode</span>: <span class="primary">forwards</span>;'
+  // + '\n\t  <span class="character">     -o-animation-fill-mode</span>: <span class="primary">forwards</span>;'
+  // + '\n\t  <span class="character">        animation-fill-mode</span>: <span class="primary">forwards</span>;'
+  // + '\n\t}';
+
+  // var htmlBG = ''
+  // +'\n\n<span class="control">&lt;!</span><span class="tag">DOCTYPE </span><span class="character">html</span><span class="control">&gt;</span>'
+  // +'\n'
+  // +'\n'
+  // +'\n<span class="control">&lt;</span><span class="tag">html</span><span class="control">&gt;</span>'
+  // +'\n<span class="control">&lt;</span><span class="tag">head</span><span class="control">&gt;</span>'
+  // +'\n  <span class="control">&lt;</span><span class="tag">title</span><span class="control">&gt;</span>Natecbd<span class="control">&lt;/</span><span class="tag">title</span><span class="control">&gt;</span>'
+  // +'\n  <span class="control">&lt;</span><span class="tag">meta</span> <span class="character">name</span>=<span class="primary">"viewport"</span> <span class="character">content</span>=<span class="primary">"width=device-width, initial-scale=1, user-scalable=no"</span><span class="control">&gt;</span>'
+  // +'\n  '
+  // +'\n  <span class="control">&lt;</span><span class="tag">link</span> <span class="character">rel</span>=<span class="primary">"stylesheet"</span> <span class="character">type</span>=<span class="primary">"text/css"</span><span class="character"> href</span>=<span class="primary">"styles/nav.css"</span><span class="control">&gt;</span>'
+  // +'\n  <span class="control">&lt;</span><span class="tag">link</span> <span class="character">rel</span>=<span class="primary">"stylesheet"</span> <span class="character">type</span>=<span class="primary">"text/css"</span> <span class="character">href</span>=<span class="primary">"styles/index.css"</span><span class="control">&gt;</span>'
+  // +'\n  <span class="control">&lt;</span><span class="tag">link</span> <span class="character">rel</span>=<span class="primary">"stylesheet"</span> <span class="character">type</span>=<span class="primary">"text/css"</span> <span class="character">href</span>=<span class="primary">"styles/normalize.css"</span><span class="control">&gt;</span>'
+  // +'\n  <span class="control">&lt;</span><span class="tag">link</span> <span class="character">rel</span>=<span class="primary">"stylesheet"</span> <span class="character">type</span>=<span class="primary">"text/css"</span> <span class="character">href</span>=<span class="primary">"styles/background.css"</span><span class="control">&gt;</span>'
+  // +'\n<span class="control">&lt;/</span><span class="tag">head</span><span class="control">&gt;</span>'
+  // +'\n<span class="control">&lt;</span><span class="tag">body</span><span class="control">&gt;</span>'
+  // +'\n  <span class="control">&lt;</span><span class="tag">div</span> <span class="character">class</span>=<span class="primary">"bg"</span><span class="control">&gt;</span>'
+  // +'\n    <span class="control">&lt;</span><span class="tag">span</span> <span class="character">class</span>=<span class="primary">"row"</span><span class="control">&gt;</span><span class="control">&lt;/</span><span class="tag">span</span><span class="control">&gt;</span>'
+  // +'\n    <span class="control">&lt;</span><span class="tag">pre</span> <span class="character">id</span>=<span class="primary">"cssBG"</span><span class="control">&gt;</span><span class="control">&lt;/</span><span class="tag">pre</span><span class="control">&gt;</span>'
+  // +'\n    <span class="control">&lt;</span><span class="tag">pre</span> <span class="character">id</span>=<span class="primary"<span><span class="control">&gt;</span><span class="control">&lt;/</span><span class="tag">pre</span><span class="control">&gt;</span>'
+  // +'\n    <span class="control">&lt;</span><span class="tag">pre</span> <span class="character">id</span>=<span class="primary">"jsBG"</span><span class="control">&gt;</span><span class="control">&lt;/</span><span class="tag">pre</span><span class="control">&gt;</span>'
+  // +'\n    <span class="control">&lt;</span><span class="tag">nav</span> <span class="character">class</span>=<span class="primary">"nav-bar"</span><span class="control">&gt;</span>'
+  // +'\n      <span class="control">&lt;</span><span class="tag">li</span> <span class="character">class</span>=<span class="primary">"nav-item hvr-underline-reveal"</span><span class="control">&gt;</span>'
+  // +'\n        <span class="control">&lt;</span><span class="tag">a</span> <span class="character">class</span>=<span class="primary">"nav-link navc fade-in-up"</span> <span class="character">href</span>=<span class="primary">"contact"</span><span class="control">&gt;</span>Contact<span class="control">&lt;/</span><span class="tag">a</span><span class="control">&gt;</span>'
+  // +'\n      <span class="control">&lt;/</span><span class="tag">li</span><span class="control">&gt;</span>'
+  // +'\n      <span class="control">&lt;</span><span class="tag">li</span> <span class="character">class</span>=<span class="primary">"nav-item hvr-underline-reveal"</span><span class="control">&gt;</span>'
+  // +'\n        <span class="control">&lt;</span><span class="tag">a</span> <span class="character">class</span>=<span class="primary">"nav-link navb fade-in-up"</span> <span class="character">href</span>=<span class="primary">"about"</span><span class="control">&gt;</span>About<span class="control">&lt;/</span><span class="tag">a</span><span class="control">&gt;</span>'
+  // +'\n      <span class="control">&lt;/</span><span class="tag">li</span><span class="control">&gt;</span>'
+  // +'\n    <span class="control">&lt;/</span><span class="tag">nav</span><span class="control">&gt;</span>'
+  // +'\n    <span class="control">&lt;</span><span class="tag">div</span> <span class="character">id</span>=<span class="primary">"page-build-panel"</span> <span class="character">class</span>=<span class="primary">""</span><span class="control">&gt;</span>'
+  // +'\n      <span class="control">&lt;</span><span class="tag">div</span> <span class="character">id</span>=<span class="primary">"page-build"</span> <span class="character">class</span>=<span class="primary">"row"</span><span class="control">&gt;</span>'
+  // +'\n        <span class="control">&lt;</span><span class="tag">a</span> <span class="character">class</span>=<span class="primary">"col-sm-1" </span><span class="character">href</span>=<span class="primary">"index.html"</span><span class="control">&gt;</span>'
+  // +'\n          <span class="control">&lt;</span><span class="tag">h3</span><span class="control">&gt;</span><span class="tag">&amp;lt;</span><span class="control">&lt;/</span><span class="tag">h3</span><span class="control">&gt;</span>'
+  // +'\n        <span class="control">&lt;/</span><span class="tag">a</span><span class="control">&gt;</span>'
+  // +'\n        <span class="control">&lt;</span><span class="tag">h3</span> <span class="character">class</span>=<span class="primary">"col-sm-10"</span><span class="control">&gt;</span>'
+  // +'\n          <span class="control">&lt;</span><span class="tag">b</span><span class="control">&gt;</span>This page is built with <span class="tag">&amp;lt;</span>/html<span class="tag">&amp;gt;</span>, CSS, <span class="tag">&amp;amp;</span> Javascript.<span class="control">&lt;/</span><span class="tag">b</span><span class="control">&gt;</span>'
+  // +'\n        <span class="control">&lt;/</span><span class="tag">h3</span><span class="control">&gt;</span>'
+  // +'\n        <span class="control">&lt;</span><span class="tag">a</span> <span class="character">class</span>=<span class="primary">"col-sm-1" </span><span class="character">href</span>=<span class="primary">"index.html"</span><span class="control">&gt;</span>'
+  // +'\n          <span class="control">&lt;</span><span class="tag">h3</span><span class="control">&gt;</span><span class="tag">&amp;gt;</span><span class="control">&lt;/</span><span class="tag">h3</span><span class="control">&gt;</span>'
+  // +'\n        <span class="control">&lt;/</span><span class="tag">a</span><span class="control">&gt;</span>'
+  // +'\n      <span class="control">&lt;/</span><span class="tag">div</span><span class="control">&gt;</span>'
+  // +'\n    <span class="control">&lt;/</span><span class="tag">div</span><span class="control">&gt;</span>'
+  // +'\n  <span class="control">&lt;/</span><span class="tag">div</span><span class="control">&gt;</span>'
+  // +'\n  <span class="control">&lt;</span><span class="tag">script</span> <span class="character">src</span>=<span class="primary">"scripts/bootstrap.js"</span><span class="control">&gt;</span><span class="control">&lt;/</span><span class="tag">script</span><span class="control">&gt;</span>'
+  // +'\n<span class="control">&lt;/</span><span class="tag">body</span><span class="control">&gt;</span>'
+  // +'\n<span class="control">&lt;/</span><span class="tag">html</span><span class="control">&gt;</span><span id="HTMLCursor">|</span>';
+
+  // var javaBG = ''
+  // +'\n\n<span class="tag">function</span> <span class="target">type</span>(<span class="character">code</span>, <span class="character">i</span>, <span class="character">background</span>, <span class="character">target</span>, <span class="character">blinker</span>) {'
+  // +'\n  <span class="tag">var</span> <span class="character">char</span> = <span class="primary">""</span>;'
+  // +'\n  <span class="tag">var</span> <span class="character">nextChar</span> = <span class="character">code</span>.<span class="target">charAt</span>(<span class="character">i</span>);'
+  // +'\n  <span class="logic">if</span>(<span class="character">nextChar</span> == <span class="primary">"<"</span>) {'
+  // +'\n    <span class="logic">for</span>(<span class="character">i</span>; <span class="character">code</span>.<span class="target">charAt</span>(<span class="character">i</span>) != <span class="primary">">"</span>; <span class="character">i</span>++) {'
+  // +'\n      <span class="character">char</span> += <span class="character">code</span>.<span class="target">charAt</span>(<span class="character">i</span>);'
+  // +'\n    }'
+  // +'\n    <span class="character">char</span> += <span class="primary">">"</span>;'
+  // +'\n  } <span class="logic">else</span> {'
+  // +'\n    <span class="character">char</span> = <span class="character">nextChar</span>;'
+  // +'\n  }'
+  // +'\n  <span class="character">background</span> += <span class="character">char</span>;'
+  // +'\n  <span class="character">target</span>.<span class="character">innerHTML</span> = <span class="character">background</span>+(<span class="character">char</span>==<span class="primary">"</span><span class="value">\\t</span><span class="primary">"</span>||<span class="character">char</span>==<span class="primary">"</span><span class="value">\\n</span><span class="primary">"</span>?<span class="primary">""</span>:<span class="primary">"|"</span>);'
+  // +'\n  <span class="logic">if</span>(<span class="character">i</span> < <span class="character">code</span>.<span class="character">length</span>) {'
+  // +'\n    <span class="target">setTimeout</span>(() <span class="tag">=></span> {'
+  // +'\n      <span class="target">type</span>(<span class="character">code</span>, <span class="character">i</span>+<span class="value">1</span>, <span class="character">background</span>, <span class="character">target</span>, <span class="character">blinker</span>);'
+  // +'\n    }, <span class="value">5</span>);'
+  // +'\n  } <span class="logic">else</span> {'
+  // +'\n    <span class="character">target</span>.<span class="character">innerHTML</span> = <span class="character">background</span>;'
+  // +'\n    <span class="target">blink</span>(<span class="character">document</span>.<span class="target">getElementById</span>(<span class="character">blinker</span>));'
+  // +'\n    <span class="tag">function</span> <span class="target">blink</span>(<span class="character">target</span>) {'
+  // +'\n      <span class="logic">if</span> (<span class="character">target</span>.<span class="character">style</span>.<span class="character">display</span> === <span class="primary">"none"</span>) {'
+  // +'\n        <span class="character">target</span>.<span class="character">style</span>.<span class="character">display</span> = <span class="primary">"inline-block"</span>;'
+  // +'\n      } <span class="logic">else</span> {'
+  // +'\n        <span class="character">target</span>.<span class="character">style</span>.<span class="character">display</span> = <span class="primary">"none"</span>;'
+  // +'\n      }'
+  // +'\n      <span class="target">setTimeout</span>(() <span class="tag">=></span> {'
+  // +'\n        <span class="target">blink</span>(<span class="character">target</span>);'
+  // +'\n      }, <span class="value">500</span>);'
+  // +'\n    }'
+  // +'\n  }'
+  // +'\n}';
+  
+  // function type(code, i, background, target, blinker) {
+  //   var char = "";
+  //   var nextChar = code.charAt(i);
+  //   if(nextChar == '<') {
+  //     for(i; code.charAt(i) != '>'; i++) {
+  //       char += code.charAt(i);
+  //     }
+  //     char += '>';
+  //   } else {
+  //     char = nextChar;
+  //   }
+  //   background += char;
+
+  //   // target.append(char);//+char=="\t"||char=="\n"?"":"<span class='cursor' id='"+blinker+"'>|</span>"
+  //   target.innerHTML = background+(char=="\t"||char=="\n"?"":"<span class='cursor'>|</span>");
+  //   // byId(blinker).remove();
+  //   if(i < code.length) {
+  //     setTimeout(function() {
+  //       type(code, i+1, background, target, blinker)
+  //     },  Math.floor(Math.random() * 4) * Math.floor(Math.random() * 30));
+  //   } else {
+  //     target.innerHTML = background
+  //     if(blinker == "HTMLCursor") {
+  //       blink(byId(blinker))
+  //       function blink(target) {
+  //         if (target.style.display === "none") {
+  //           target.style.display = "inline-block";
+  //         } else {
+  //           target.style.display = "none";
+  //         }
+  //         setTimeout(function() {
+  //           blink(target);
+  //         }, 500);
+  //       }
+  //     }
+  //   }
+  // }
+
+  // type(cssBG,  0, "", byId("cssBG"),  "CSSCursor")
+  // typ , "", byId) "JavaCursor")
+  // type(htmlBG, 0, "", byId("htmlBG"), "HTMLCursor")
+
+  // var preBlock = byId("preBlock");
+
